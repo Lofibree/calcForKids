998a2feb68aea92363eedb8b46cc07a34a57b6fb
Lofibree <nastenkapersik17@gmail.com>
2022-10-04 10:23:53 +0300

    second

---
+++ calcKids.js
@@ -0,0 +1,17 @@
+const userInput = document.getElementById('userInput');
+var expression = '';
+
+const press = (input) => {
+    expression += input;
+    userInput.value = expression;
+}
+
+const equal = () => {
+    userInput.value = eval(expression);
+    expression = '';
+}
+
+const erase = () => {
+    expression = '';
+    userInput.value = expression;
+}
